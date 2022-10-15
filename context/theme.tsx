f85cd66da77284e53218ec3239a2f477feef1298
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-10-15 17:37:10 +0100

    Bug fix.

--- context/theme.tsx
+++ context/theme.tsx
@@ -1,8 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-const ThemeContext = createContext<
-  [{ theme: string; themeToggle: () => void }] | undefined
->(undefined);
+const ThemeContext = createContext(undefined);
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("dark");
