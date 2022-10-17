40011072a5691d274e73aac3fe401b231cb5d1c5
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-10-17 17:59:51 +0100

    Updated Navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="py-[24px] lg:py-3 px-[24px] lg:px-0 lg:w-[70%] mx-auto flex items-center justify-between relative z-10">
         <h1 className="font-medium text-3xl font-lufga">DYOR</h1>
         <nav
-          className={`fixed lg:static bg-secondary lg:bg-inherit min-h-[400px] lg:min-h-0 border-2 lg:border-0 rounded-xl border-white h-auto w-[80%] lg:w-auto right-[24px] top-[150px] z-[9] flex-col lg:flex-row flex gap-10 lg:gap-20 p-[24px] lg:p-0 justify-center lg:justify-start transition duration-500 ${
+          className={`fixed lg:static bg-neutral-800 lg:bg-inherit min-h-[400px] lg:min-h-0 border-2 lg:border-0 rounded-xl border-white h-auto w-[80%] lg:w-auto right-[24px] top-[150px] z-[9] flex-col lg:flex-row flex gap-10 lg:gap-20 p-[24px] lg:p-0 items-center justify-center lg:justify-start lg:items-start transition duration-500 ${
             isNavOpen ? "translate-y-0" : "-translate-y-[150%] lg:translate-y-0"
           }`}
         >
