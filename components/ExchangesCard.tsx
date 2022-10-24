e682b27b8b2b5a7222a2bd07fc1b4bd89063bce1
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-10-24 08:55:35 +0100

    Fixed bugs.

--- components/ExchangesCard.tsx
+++ components/ExchangesCard.tsx
@@ -10,9 +10,9 @@ const ExchangesCard = ({
 }: ExchangesProps) => {
   return (
     <Link href={url} target="_blank">
-      <div className="rounded-xl p-[12px] dark:bg-neutral-700 bg-white hover:bg-brand-gray-200 shadow-lg dark:shadow-none dark:hover:bg-neutral-800 transition-colors duration-500 cursor-pointer text-center w-[200px] xsm:w-[170px] ssm:w-[185px] nsm:w-[160px] md:w-[180px] lg:min-w-[200px] h-[200px] max-h-[200px]">
+      <div className="rounded-xl p-[12px] dark:bg-neutral-700 bg-white hover:bg-brand-gray-200 shadow-lg dark:shadow-none dark:hover:bg-neutral-800 transition-colors duration-500 cursor-pointer text-center w-[200px] xsm:w-[170px] ssm:w-[185px] nsm:w-[160px] md:w-[180px] lg:min-w-[200px] h-[200px] max-h-[200px] flex flex-col justify-between">
         <h1 className="font-lufga text-2xl">{name}</h1>
-        <div className="w-full my-4">
+        <div className="w-full">
           {img && <Image src={img} width={50} height={50} />}
         </div>
         <h3>{`${volume.toFixed(0)} BTC`}</h3>
