3aa83e9f84e5bc6dea96aac47301455161b6447c
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-10-31 16:21:15 +0530

    added next/link to avoid refresh

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const NavLink = ({ link, text }) => {
   return (
     <li
       className={`relative after:absolute dark:hover:text-brand-gray hover:after:w-full after:transition-[width] after:duration-500 after:w-0 after:h-[2px] after:bg-secondary dark:after:bg-white after:left-0 after:-bottom-[5px]`}
     >
-      <a href={link} className="text-3xl lg:text-[14px]">
-        {text}
-      </a>
+      <Link href={link}>
+        <a className="text-3xl lg:text-[14px]">{text}</a>
+      </Link>
     </li>
   );
 };
